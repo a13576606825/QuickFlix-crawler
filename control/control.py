7396ed34b117f11f47783ada3e3aede1bdcde107
Yang Kenneth Lu <lu.yang.kenneth@gmail.com>
2016-10-16 18:21:18 +0800

    Added parseReview() and BeautifulSoup dependency

--- control/control.py
+++ control/control.py
@@ -16,5 +16,11 @@ log = logging.getLogger(__name__)
 def start():
     #store.write.insert_one('test', {'number':1})
 
-    data = remote.crawler.fetch('http://variety.com/2016/film/reviews/the-magnificent-seven-review-toronto-film-festival-denzel-washington-chris-pratt-1201854625/')
-    log.info(data)
+    # TODO
+    url = 'http://variety.com/2016/film/reviews/the-magnificent-seven-review-toronto-film-festival-denzel-washington-chris-pratt-1201854625/';
+    html = remote.crawler.fetch(url)
+    review = remote.crawler.parseReview(html)
+    if review is None:
+    	log.info('Page does not contain a movie review json')
+    else:
+    	log.info(review['author'][0]['name'])
--- remote/crawler.py
+++ remote/crawler.py
@@ -7,21 +7,48 @@ import logging
 
 import requests
 import re
+import json
+from bs4 import BeautifulSoup
 
 log = logging.getLogger(__name__)
-regex = re.compile('(text\/html|application\/xhtml\+xml|text\/xml|application\/xml).*')
+regex_html = re.compile('(text\/html|application\/xhtml\+xml).*')
+regex_json = re.compile('{.*}')
 
 
 '''
 Arg(s): url
-Return: if html document, its text body
-		else None
+
+if host gives a html document, return its text body
+else return None
 '''
 def fetch(url):
 	r = requests.head(url) # TODO implement a timeout and catch exceptions
-	if regex.match(r.headers['content-type']) is None:
+
+	if regex_html.match(r.headers['content-type']) is None:
 		return None
 	else:
 		r = requests.get(url) # TODO implement a timeout and catch exceptions
 		return r.text
+
+'''
+Arg(s): text body of html document
+
+if movie review json exists, return the json object
+else return None
+'''
+def parseReview(html):
+	soup = BeautifulSoup(html, 'html.parser')
+
+	for script_block in soup.find_all('script', attrs={'type': 'application/ld+json'}):
+		review_string = script_block.string
+		find_json = regex_json.search(review_string)
+
+		if find_json is not None:
+			review_string = find_json.group(0)
+			review = json.loads(review_string) # TODO catch exception
+
+			if review.has_key('@type') and review.has_key('itemReviewed') and review['itemReviewed'].has_key('@type') and review['@type'] == 'Review' and review['itemReviewed']['@type'] == 'Movie':
+				return review;
+	
+	return None
 
