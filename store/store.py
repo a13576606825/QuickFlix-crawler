ddbb0ff32fc2adfe17b96da196bbb20c88f4177d
Yang Kenneth Lu <lu.yang.kenneth@gmail.com>
2016-10-18 19:25:16 +0800

    Added functions for integration with front-end
    * Added add_movie()
    * Added get_movies()
    * Modified add_review()

--- store/store.py
+++ store/store.py
@@ -10,20 +10,47 @@ from bson.objectid import ObjectId
 log = logging.getLogger(__name__)
 
 
-def add_review(review):
+def add_movie(movie_title):
     db = mongo.get_db()
-    item_id = db.reviews.insert_one(review).inserted_id
+    item_id = db.movies.insert_one({'title': movie_title}).inserted_id
     if item_id == 0:
-        log.error("Unable to add selected review")
-        return 0
+        log.error("Unable to add movie title")
+        return None
     return item_id
 
 
+# Returns a list of all movie titles
+def get_movies():
+    db = mongo.get_db()
+    movies = list(db.movies.find())
+    if movies:
+        for i, item in enumerate(movies):
+            movies[i] = item['title']
+    return movies
+
+
+def add_review(review):
+    db = mongo.get_db()
+
+    # If movie title is not already in db.movies, add it
+    movie_title = review['itemReviewed']['name'] # TODO catch exception (malformed json)
+    movies = list(db.movies.find({'title': movie_title}))
+    if not movies:
+        add_movie(movie_title)
+
+    # Add review into db.reviews
+    item_id = db.reviews.insert_one(review).inserted_id
+    if item_id == 0:
+        log.error("Unable to add review")
+        return None
+    return item_id
+
+
+# Returns a list of all reviews (json objects) for a particular movie
 def get_reviews(movie_title):
     db = mongo.get_db()
     reviews = list(db.reviews.find({'itemReviewed.name': movie_title}))
     if not reviews:
-        reviews = []
         log.info("No reviews found for " + movie_title)
     return reviews
 
@@ -32,8 +59,8 @@ def queue_push(url):
     db = mongo.get_db()
     item_id = db.queue.insert_one({'url': url}).inserted_id
     if item_id == 0:
-        log.error("Unable to add selected URL into queue")
-        return 0
+        log.error("Unable to add URL into queue")
+        return None
     return item_id
 
 
@@ -51,8 +78,8 @@ def add_to_visited(url):
     db = mongo.get_db()
     item_id = db.visited.insert_one({'url': url}).inserted_id
     if item_id == 0:
-        log.error("Unable to add selected URL into visited")
-        return 0
+        log.error("Unable to add URL into visited")
+        return None
     return item_id
 
 
