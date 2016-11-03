9400d9e15e37f380bacc931ff963c953c65e30d8
Zhang Ji <zhangji951027@gmail.com>
2016-11-03 22:42:00 +0800

    update seed urls

--- control/control.py
+++ control/control.py
@@ -15,23 +15,32 @@ from remote import remote
 
 log = logging.getLogger(__name__)
 
-
+SEED_FILE_PATH = 'seed.txt'
 THREAD_PAUSE_TIME = 1 # seconds
 
 ''' Top Level Method '''
 def start():
-    store.empty_db() # TODO uncommented for now
+    # store.empty_db() # TODO uncommented for now
 
-    # Seed URLs
-    urls = [
-    	'http://variety.com/2016/film/reviews/the-magnificent-seven-review-toronto-film-festival-denzel-washington-chris-pratt-1201854625/',
-    	'http://www.empireonline.com/movies/magnificent-seven-2/review/',
-    	'http://www.metacritic.com/movie/the-magnificent-seven',
-    	'http://www.newyorker.com/goings-on-about-town/movies/the-magnificent-seven',
-    	'http://www.nytimes.com/2016/09/23/movies/magnificent-seven-review-denzel-washington.html',
-    	'http://www.avclub.com/review/magnificent-seven-gets-uninspired-remake-242722',
-    	'http://variety.com/author/owen-gleiberman/'
-    ] # TODO uncommented for now
+    # # Seed URLs
+    # urls = [
+    # 	'http://variety.com/2016/film/reviews/the-magnificent-seven-review-toronto-film-festival-denzel-washington-chris-pratt-1201854625/',
+    # 	'http://www.empireonline.com/movies/magnificent-seven-2/review/',
+    # 	'http://www.nytimes.com/2016/09/23/movies/magnificent-seven-review-denzel-washington.html',
+    # 	'http://www.avclub.com/review/magnificent-seven-gets-uninspired-remake-242722',
+    # ] # TODO uncommented for now
+    # urls = [
+
+    # ] # TODO uncommented for now
+    f = open(SEED_FILE_PATH, 'r')
+    urls = f.read().splitlines()
+
+    for url in urls:
+        if remote.checkValidity(url):
+            print("seeeeeeee:      "+url)
+        else:
+            print('oooooooops')
+    return
 
     for url in urls:
         # give those urls highest priority
@@ -66,3 +75,4 @@ def _single_crawler(thread_name):
             print('[%s] (BUG!!!)Unhandled exception occurs' % thread_name)
             print(e)
         time.sleep(THREAD_PAUSE_TIME)
+        break;
